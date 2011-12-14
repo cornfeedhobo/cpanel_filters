d307dc63f6f64bdc1f40310132c4efeb89faf3a9
Cornfeed Hobo <jdough823@gmail.com>
2011-12-14 04:40:51 -0600

    More Validation, Roundcube coding standards, cleanup

--- cpanel_filters.js
+++ cpanel_filters.js
@@ -17,8 +17,13 @@ if (window.rcmail) {
             
              if (rcmail.gui_objects.filterlist) {
                 var p = rcmail;
-                rcmail.filterlist = new rcube_list_widget( rcmail.gui_objects.filterlist, {multiselect:false, draggable:false, keyboard:false} );
-                rcmail.filterlist.addEventListener('select', function(o) {p.cpf_select(o);} );
+                rcmail.filterlist = new rcube_list_widget(
+                    rcmail.gui_objects.filterlist, {
+                        multiselect:false, draggable:false, keyboard:false} );
+                rcmail.filterlist.addEventListener('select', function(o)
+                {
+                    p.cpf_select(o);
+                } );
                 rcmail.filterlist.init();
                 rcmail.filterlist.focus();
             }
@@ -57,50 +62,60 @@ if (window.rcmail) {
 /*********       cPanel Filters UI methods       *********/
 /*********************************************************/
 // Select list item handler
-rcube_webmail.prototype.cpf_select = function(list) {
+rcube_webmail.prototype.cpf_select = function(list)
+{
     var id = list.get_single_selection();
     if (id != null)
         this.cpf_frame(list.rows[id].uid, 'plugin.cpanel_filters-edit');
 };
 // Load the framed filteredit
-rcube_webmail.prototype.cpf_frame = function(fid, action) {
+rcube_webmail.prototype.cpf_frame = function(fid, action)
+{
     if ( this.env.contentframe && window.frames && window.frames[this.env.contentframe] ) {
         target = window.frames[this.env.contentframe];
         var msgid = this.set_busy(true, 'loading');
         target.location.href = this.env.comm_path+'&_action='+action+'&_framed=1&_fid='+fid+'&_unlock='+msgid;
     }
 }
-rcube_webmail.prototype.cpf_ruleadd = function(bttnrow) {
+rcube_webmail.prototype.cpf_ruleadd = function(bttnrow)
+{
     this.http_post('plugin.cpanel_filters', '_act=ruleadd&_rid='+bttnrow);
 };
-rcube_webmail.prototype.cpf_actionadd = function(bttnrow) {
+rcube_webmail.prototype.cpf_actionadd = function(bttnrow)
+{
     this.http_post('plugin.cpanel_filters', '_act=actionadd&_aid='+bttnrow);
 };
-rcube_webmail.prototype.cpf_ruledel = function(rid){
+rcube_webmail.prototype.cpf_ruledel = function(rid)
+{
     if (confirm(this.get_label('cpanel_filters.filterRuledelete'))) {
         $('div#rulerow'+rid).remove();
     }
     rcmail.cpf_formbuttons('rulerow');
 };
-rcube_webmail.prototype.cpf_actiondel = function(aid){
+rcube_webmail.prototype.cpf_actiondel = function(aid)
+{
     if (confirm(this.get_label('cpanel_filters.filterActiondelete'))) {
         $('div#actionrow'+aid).remove();
     }
     rcmail.cpf_formbuttons('actionrow');
 };
-rcube_webmail.prototype.cpf_insertrow = function(content, bttnrow, field) {
+rcube_webmail.prototype.cpf_insertrow = function(content, bttnrow, field)
+{
     if ( content != '' ) {
         $('div#'+field+bttnrow).after(content);
     }
     rcmail.cpf_formbuttons(field);
 };
-rcube_webmail.prototype.cpf_formbuttons = function(field) {
+rcube_webmail.prototype.cpf_formbuttons = function(field)
+{
     // Rename form elements
     if ( field != null ) {
         if ( field == 'rulerow' || field == 'both' ) {
-            $('div.rulerow').each(function(i){$(this).attr('id', 'rulerow'+i);});
-            $('select[name$="[part]"]').each(function(i){
-                $(this).change(function() {
+            $('div.rulerow').each( function(i) { $(this).attr('id', 'rulerow'+i); } );
+            $('select[name$="[part]"]').each( function(i)
+            {
+                $(this).change(function()
+                {
                     if ( ( $(this).val() == '$header_from:' ||
                            $(this).val() == '$header_to:' ||
                            $(this).val() == '$header_from:' ||
@@ -109,15 +124,18 @@ rcube_webmail.prototype.cpf_formbuttons = function(field) {
                            $('select#match'+i).val() == 'is' ) {
                         
                         $('input#value'+i).addClass('email');
-                    } else if ( $(this).val() != 'is' ) {
+                    }
+                    else if ( $(this).val() != 'is' ) {
                         $('input#value'+i).removeClass('email');
                     }
                 } );
                 $(this).attr('name', '_rules['+i+'][part]');
                 $(this).attr('id', 'header'+i);
             });
-            $('select[name$="[match]"]').each(function(i){
-                $(this).change(function() {
+            $('select[name$="[match]"]').each( function(i)
+            {
+                $(this).change(function()
+                {
                     if ( $(this).val() == 'is' && (
                         $('select#header'+i).val() == '$header_from:' ||
                         $('select#header'+i).val() == '$header_to:' ||
@@ -126,45 +144,56 @@ rcube_webmail.prototype.cpf_formbuttons = function(field) {
                         $('select#header'+i).val() == 'foranyaddress $h_to:,$h_cc:,$h_bcc:' ) ) {
                         
                         $('input#value'+i).addClass('email');
-                    } else if ( $(this).val() != 'is' ) {
+                    }
+                    else if ( $(this).val() != 'is' ) {
                         $('input#value'+i).removeClass('email');
                     }
                 } );
                 $(this).attr('name', '_rules['+i+'][match]');
                 $(this).attr('id', 'match'+i);
-            });
-            $('input[name$="[val]"]').each(function(i){
+            } );
+            $('input[name$="[val]"]').each( function(i)
+            {
                 $(this).attr('name', '_rules['+i+'][val]');
                 $(this).attr('id', 'value'+i);
-            });
-            $('select[name$="[opt]"]').each(function(i){
+            } );
+            $('select[name$="[opt]"]').each( function(i)
+            {
                 $(this).attr('name', '_rules['+i+'][opt]');
                 $(this).attr('id', 'opt'+i);
-            });
-            $('input[id^="ruleadd"]').each(function(i){
-                //$(this).on('click','input[id^="ruleadd"]',function(event){rcmail.cpf_ruleadd(i)});
-                $(this)[0].onclick = function(){rcmail.cpf_ruleadd(i);};
+            } );
+            $('input[id^="ruleadd"]').each( function(i)
+            {
+                $(this)[0].onclick = function() { rcmail.cpf_ruleadd(i); };
                 $(this).attr('id', 'ruleadd'+i);
-            });
-            $('input[id^="ruledel"]').each(function(i){
-                $(this)[0].onclick = function(){rcmail.cpf_ruledel(i);};
+            } );
+            $('input[id^="ruledel"]').each( function(i)
+            {
+                $(this)[0].onclick = function() { rcmail.cpf_ruledel(i); };
                 $(this).attr('id', 'ruledel'+i);
                 if ( $('div.rulerow').length < 2 ) {
                     $(this).addClass('disabled');
                     $(this).attr('disabled','disabled');
-                } else if ( $('div.rulerow').length > 1 ) {
+                }
+                else if ( $('div.rulerow').length > 1 ) {
                     $(this).removeClass('disabled');
                     $(this).removeAttr('disabled');
                 }
-            });
-            $('select[name$="[opt]"]').each(function(i){
-                if(($('div.rulerow').length-1)>i){$(this).show();}else{$(this).hide();}
-            });
+            } );
+            $('select[name$="[opt]"]').each( function(i)
+            {
+                if ( ( $('div.rulerow').length - 1 ) > i )
+                    $(this).show();
+                else
+                    $(this).hide();
+            } );
         }
         if ( field == 'actionrow' || field == 'both' ) {
-            $('div.actionrow').each(function(i){$(this).attr('id', 'actionrow'+i);});
-            $('select[name$="[action]"]').each(function(i){
-                $(this).change(function() {
+            $('div.actionrow').each( function(i) { $(this).attr('id', 'actionrow'+i); } );
+            $('select[name$="[action]"]').each( function(i)
+            {
+                $(this).change( function()
+                {
                     if ( $(this).val() == 'deliver' || $(this).val() == 'fail' ) {
                         $('select#mailbox'+i).hide();
                         $('input#dest'+i).show();
@@ -173,48 +202,55 @@ rcube_webmail.prototype.cpf_formbuttons = function(field) {
                             $('input#dest'+i).addClass('email');
                         else if ( $(this).val() == 'fail' )
                             $('input#dest'+i).removeClass('email');
-                    } else if ( $(this).val() == 'save' ) {
+                    }
+                    else if ( $(this).val() == 'save' ) {
                         $('input#dest'+i).hide();
                         $('select#mailbox'+i).show();
                         $('input#dest'+i).removeClass('required email');
                     }
-                });
+                } );
                 $(this).attr('name', '_actions['+i+'][action]');
                 $(this).attr('id', 'action'+i);
-            });
-            $('input[name$="[dest]"]').each(function(i){
+            } );
+            $('input[name$="[dest]"]').each( function(i)
+            {
                 $(this).attr('name', '_actions['+i+'][dest]');
                 $(this).attr('id', 'dest'+i);
-            });
-            $('select[name$="[folder]"]').each(function(i){
+            } );
+            $('select[name$="[folder]"]').each( function(i)
+            {
                 $(this).attr('name', '_actions['+i+'][folder]');
                 $(this).attr('id', 'mailbox'+i);
-            });
-            $('input[id^="actionadd"]').each(function(i){
-                //$(this).on('click','input[id^="ruleadd"]',function(event){rcmail.cpf_ruleadd(i)});
+            } );
+            $('input[id^="actionadd"]').each( function(i)
+            {
                 $(this)[0].onclick = function(){rcmail.cpf_actionadd(i);};
                 $(this).attr('id', 'actionadd'+i);
-            });
-            $('input[id^="actiondel"]').each(function(i){
-                $(this)[0].onclick = function(){rcmail.cpf_actiondel(i);};
+            } );
+            $('input[id^="actiondel"]').each( function(i)
+            {
+                $(this)[0].onclick = function() { rcmail.cpf_actiondel(i); };
                 $(this).attr('id', 'actiondel'+i);
                 if ( $('div.actionrow').length < 2 ) {
                     $(this).addClass('disabled');
                     $(this).attr('disabled','disabled');
-                } else if ( $('div.actionrow').length > 1 ) {
+                }
+                else if ( $('div.actionrow').length > 1 ) {
                     $(this).removeClass('disabled');
                     $(this).removeAttr('disabled');
                 }
-            });
+            } );
         }
     }
 }
-rcube_webmail.prototype.cpf_reload = function(url) {
+rcube_webmail.prototype.cpf_reload = function(url)
+{
     parent.rcmail.set_busy(true);
     parent.rcmail.goto_url(url);
     parent.rcmail.set_busy(false);
 }
-rcube_webmail.prototype.cpf_rowid = function(id) {
+rcube_webmail.prototype.cpf_rowid = function(id)
+{
     var i, rows = parent.rcmail.filterlist.rows;
     for (i=0; i<rows.length; i++)
         if (rows[i] != null && rows[i].uid == id)
--- cpanel_filters.php
+++ cpanel_filters.php
@@ -12,7 +12,8 @@
  * @copyright 2008-2011, The Roundcube Dev Team
  */
 
-class cpanel_filters extends rcube_plugin {
+class cpanel_filters extends rcube_plugin
+{
     public $task = 'settings';
     private $rcmail; // RC class
     private $xmlapi; //cpanel xmlapi class
@@ -48,15 +49,20 @@ class cpanel_filters extends rcube_plugin {
     /**
      * Roundcube init handler (mandatory)
      */
-    function init() {
+    function init()
+    {
         // add Tab label/title
         $this->add_texts('localization/', true);
         
         // register javascript actions
-        $this->register_action('plugin.cpanel_filters',array($this,'cpanel_filters_actions'));
-        $this->register_action('plugin.cpanel_filters-add',array($this,'cpanel_filters_actions'));
-        $this->register_action('plugin.cpanel_filters-edit',array($this,'cpanel_filters_actions'));
-        $this->register_action('plugin.cpanel_filters-save', array($this, 'cpanel_filters_save'));
+        $this->register_action('plugin.cpanel_filters',
+                array($this,'cpanel_filters_actions') );
+        $this->register_action('plugin.cpanel_filters-add',
+                array($this,'cpanel_filters_actions') );
+        $this->register_action('plugin.cpanel_filters-edit',
+                array($this,'cpanel_filters_actions') );
+        $this->register_action('plugin.cpanel_filters-save',
+                array($this, 'cpanel_filters_save') );
         
         // include main js script
         $this->include_script('cpanel_filters.js');
@@ -66,7 +72,8 @@ class cpanel_filters extends rcube_plugin {
      * Sets up the xmlapi connection, sets $this->filters
      * Adds UI handlers
      */
-    function _start() {
+    function _start()
+    {
         $this->rcmail = rcmail::get_instance();
         $this->load_config(); // load our config
 
@@ -99,7 +106,8 @@ class cpanel_filters extends rcube_plugin {
         $this->filters = $query['cpanelresult']['data'];
     } // end _start()
     
-    function cpanel_filters_actions() {
+    function cpanel_filters_actions()
+    {
         $this->_start();
         
         if ($action = get_input_value('_act', RCUBE_INPUT_GPC)) {
@@ -111,12 +119,14 @@ class cpanel_filters extends rcube_plugin {
                 $bttnrow = get_input_value('_rid', RCUBE_INPUT_GPC);
                 $content = $this->parse_rules($fid, 4097);
                 $this->rcmail->output->command('cpf_insertrow', $content, $bttnrow, 'rulerow');
-            } elseif ($action == 'actionadd') {
+            }
+            elseif ($action == 'actionadd') {
                 // JS will rename rows anyways, why care about id#?
                 $bttnrow = get_input_value('_aid', RCUBE_INPUT_GPC);
                 $content = $this->parse_actions($fid, 4097);
                 $this->rcmail->output->command('cpf_insertrow', $content, $bttnrow, 'actionrow');
-            } elseif ($action == 'delete') {
+            }
+            elseif ($action == 'delete') {
                 $res[] = json_decode($this->xmlapi->api2_query( $this->cuser,
                         'Email', 'deletefilter', array(
                             account     => $this->rcmail->user->get_username(),
@@ -127,18 +137,19 @@ class cpanel_filters extends rcube_plugin {
                 $msg = get_object_vars($msg['data'][0]);
                 if ( strpos( $msg['statusmsg'], 'deleted') === FALSE ) {
                     $this->rcmail->output->show_message('cpanel_filters.filterDeleteerror','error');
-                } else {
+                }
+                else {
                     $this->rcmail->output->show_message('cpanel_filters.filterDeleted','notice',null,true,6);
                     $this->rcmail->output->command('cpf_reload', 'plugin.cpanel_filters');
                 }
             }
             $this->rcmail->output->send();
         }
-        
         $this->cpanel_filters_send();
     } // end cpanel_filters_actions()
     
-    function cpanel_filters_save() {
+    function cpanel_filters_save()
+    {
         $this->_start();
         $fid            = get_input_value('_fid', RCUBE_INPUT_GPC);
         $nname          = get_input_value('_name', RCUBE_INPUT_GPC);
@@ -150,12 +161,14 @@ class cpanel_filters extends rcube_plugin {
             if ( $nname == $oname ) {
                 $save_basics['account'] = $this->rcmail->user->get_username();
                 $save_basics['filtername'] = $nname;
-            } elseif (true) {
+            }
+            elseif (true) {
                 $save_basics['account'] = $this->rcmail->user->get_username();
                 $save_basics['filtername'] = $nname;
                 $save_basics['oldfiltername'] = $oname;
             }
-        } elseif ( $nname == '' ) {
+        }
+        elseif ( $nname == '' ) {
             $this->rcmail->output->redirect( array(
                 '_task'     => 'settings',
                 '_action'   => 'plugin.cpanel_filters-edit',
@@ -171,15 +184,22 @@ class cpanel_filters extends rcube_plugin {
             if ( ( $iact['action'] == 'fail' || $iact['action'] == 'deliver') 
                     && $iact['dest'] == '' ) {
                 continue;
-            } elseif ( $iact['action'] == 'fail' || $iact['action'] == 'deliver' ) {
+            }
+            elseif ( $iact['action'] == 'fail' || $iact['action'] == 'deliver' ) {
                 $save_actions['action'.$x] = $iact['action'];
                 $save_dests['dest'.$x] = $iact['dest'];
-            } elseif ( $iact['action'] == 'save' ) {
+            }
+            elseif ( $iact['action'] == 'save' ) {
                 $save_actions['action'.$x] = $iact['action'];
                 if ( $iact['folder'] == 'INBOX' ) {
-                    $save_dests['dest'.$x] = $this->rcmail->config->get('cpanel_filters_maildir').$this->rcmail->user->get_username('local').'/'.$iact['folder'];
-                } else {
-                    $save_dests['dest'.$x] = $this->rcmail->config->get('cpanel_filters_maildir').$this->rcmail->user->get_username('local').'/.'.$iact['folder'];
+                    $save_dests['dest'.$x] = $this->rcmail->config->get('cpanel_filters_maildir') .
+                            $this->rcmail->user->get_username('local') .
+                            '/' . $iact['folder'];
+                }
+                else {
+                    $save_dests['dest'.$x] = $this->rcmail->config->get('cpanel_filters_maildir') .
+                            $this->rcmail->user->get_username('local') .
+                            '/.' . $iact['folder'];
                 }
             }
         }
@@ -189,7 +209,8 @@ class cpanel_filters extends rcube_plugin {
             $x = count($save_matches)+1;
             if ( $irule['val'] == '' ) {
                 continue;
-            } elseif ( $irule['val'] != '' ) {
+            }
+            elseif ( $irule['val'] != '' ) {
                 $save_matches['match'.$x] = $irule['match'];
                 $save_parts['part'.$x] = $irule['part'];
                 $save_values['val'.$x] = $irule['val'];
@@ -198,14 +219,16 @@ class cpanel_filters extends rcube_plugin {
         }
         $save_options['opt'.count($save_options)] = 'or';
         
-        $save = array_merge($save_basics,$save_actions,$save_dests,$save_matches,$save_parts,$save_values,$save_options);
+        $save = array_merge( $save_basics, $save_actions, $save_dests,
+                    $save_matches, $save_parts, $save_values, $save_options);
         $query = json_decode( $this->xmlapi->api2_query( $this->cuser, 'Email',
                     'storefilter', $save), true );
         
         if ( $query['cpanelresult']['data'][0]['error'] != 0 ||
                 $query['cpanelresult']['data'][0]['result'] != 'Filter Saved.' ) {
             $this->rcmail->output->show_message('cpanel_filters.filterErrorUnknown');
-        } elseif ( $query['cpanelresult']['data'][0]['result'] == 'Filter Saved.' ) {
+        }
+        elseif ( $query['cpanelresult']['data'][0]['result'] == 'Filter Saved.' ) {
             $this->rcmail->output->show_message('cpanel_filters.filterSaved');
             $this->rcmail->output->command('parent.cpf_reload','plugin.cpanel_filters');
         }
@@ -214,11 +237,13 @@ class cpanel_filters extends rcube_plugin {
     /**
      *Custom rcmail->output->send wrapper that handles the framed content
      */
-    function cpanel_filters_send() {
+    function cpanel_filters_send()
+    {
         // Handle the frame
         if (isset($_GET['_framed']) || isset($_POST['_framed'])) {
             $this->rcmail->output->send('cpanel_filters.filteredit');
-        } else {
+        }
+        else {
             $this->rcmail->output->set_pagetitle( Q( $this->gettext('filterManage') ) );
             $this->rcmail->output->send('cpanel_filters.cpanel_filters');
         }
@@ -229,7 +254,8 @@ class cpanel_filters extends rcube_plugin {
      * @param string $attrib Provided by RC's template engine
      * @return mixed XHTML output
      */
-    function html_filterlist($attrib) {
+    function html_filterlist($attrib)
+    {
         // Set the HTML ID attribute if not specified
         if (!$attrib['id'])
             $attrib['id'] = 'rcmcpflist';
@@ -252,14 +278,15 @@ class cpanel_filters extends rcube_plugin {
      * @param type $attrib Provided by RC's template engine
      * @return mixed XHTML output
      */
-    function html_filterframe($attrib) {
+    function html_filterframe($attrib)
+    {
         // Set the HTML ID attribute if not specified
         if (!$attrib['id'])
             $attrib['id'] = 'rcmcpfframe';
         $attrib['name'] = $attrib['id'];
         $this->rcmail->output->set_env('contentframe', $attrib['name']);
-        $this->rcmail->output->set_env('blankpage',
-                $attrib['src'] ? $this->rcmail->output->abs_url($attrib['src']) : 'program/blank.gif');
+        $this->rcmail->output->set_env('blankpage', $attrib['src'] ? 
+                $this->rcmail->output->abs_url($attrib['src']) : 'program/blank.gif');
         return html::tag('iframe', $attrib);
     } // end html_filterframe
     
@@ -268,7 +295,8 @@ class cpanel_filters extends rcube_plugin {
      * @param type $attr Provided by RC's template engine
      * @return mixed XHTML output
      */
-    function html_filterform($attr) {
+    function html_filterform($attr)
+    {
         // Set the HTML ID attribute if not specified
         if (!$attrib['id'])
             $attrib['id'] = 'rcmcpfform';
@@ -278,7 +306,7 @@ class cpanel_filters extends rcube_plugin {
         // When given nothing, evalutes: NULL
         $flt = $this->filters[$fid];
         
-        $out = '<form name="filterform" id="filterform" action="./" method="post">'."\n";
+        $out = '<form name="filterform" id="filterform" action="./" method="post">' . "\n";
         
         // Begin the form w\hidden fields
         $hiddenfields = new html_hiddenfield( array(
@@ -321,7 +349,8 @@ class cpanel_filters extends rcube_plugin {
                 '_name', Q( $this->gettext('filterName') ), $input_name);
         
         // Process the Rules (accounts for $fid==null)
-        $out .= '<fieldset><legend>'.Q($this->gettext('filterRules')).'</legend>'."\n";
+        $out .= '<fieldset><legend>' . Q( $this->gettext('filterRules') ) .
+                '</legend>' . "\n";
         $rows = ( $flt != null ) ? sizeof( $flt['rules'] ) : 1;
         $out .= '<div id="rules">';
         for ( $i=0; $i<$rows; $i++ )
@@ -329,7 +358,8 @@ class cpanel_filters extends rcube_plugin {
         $out .= '</div>'."\n".'</fieldset>'."\n";
         
         // Process the Actions
-        $out .= '<fieldset><legend>' . Q( $this->gettext('filterActions') ) . '</legend>'."\n";
+        $out .= '<fieldset><legend>' . Q( $this->gettext('filterActions') ) .
+                '</legend>'."\n";
         $rows = ( $flt != null ) ? sizeof($flt['actions']) : 1;
         $out .= '<div id="actions">';
         for ( $i=0; $i<$rows; $i++ )
@@ -349,9 +379,10 @@ class cpanel_filters extends rcube_plugin {
      * @param bool $div Include <div> wrapper
      * @return mixed XHTML output
      */
-    function parse_rules( $fid, $rid, $div = true ) {  // fid = NULL,rid = int(0)
-        $rule = $this->filters[$fid]['rules'][$rid];  // returns NULL
-        $rows = sizeof($this->filters[$fid]['rules']);  // returns int(0)
+    function parse_rules( $fid, $rid, $div = true )
+    {
+        $rule = $this->filters[$fid]['rules'][$rid];
+        $rows = sizeof($this->filters[$fid]['rules']);
         
         // handle div wrapper for dynamic adding
         $out = $div ? '<div class="rulerow" id="rulerow'.$rid.'">'."\n" : '';
@@ -385,9 +416,10 @@ class cpanel_filters extends rcube_plugin {
         
         $out .= '<input type="text" name="_rules[' . $rid . '][val]" id="value' .
                 $rid . '"value="' . Q($rule['val']) . '" class="required' .
-                ( ( $rule['match']=='is' && ( $rule['part']=='$header_from:' ||
+                ( ( ( $rule['match']=='is' && ( $rule['part']=='$header_from:' ||
                 $rule['part']=='$header_to:' || $rule['part']=='$reply_address:' ||
-                $rule['part']=='foranyaddress $h_to:,$h_cc:,$h_bcc:' ) ) ? ' email' : '' ) . '" size="20" />' .
+                $rule['part']=='foranyaddress $h_to:,$h_cc:,$h_bcc:' ) ||
+                !isset($rule['match']) ) ) ? ' email' : '' ) . '" size="20" />' .
                 "\n" . '</td>';
         
         // Create AND/OR <select> list
@@ -400,7 +432,8 @@ class cpanel_filters extends rcube_plugin {
         $select_opt->add( Q($this->gettext('filterOr')), Q('or') );
         $select_opt->add( Q($this->gettext('filterAnd')), Q('and') );
         if ( isset($rule['opt']) )
-            $out .= $select_opt->show( ( $rule['opt']=='and' ? Q($this->gettext('filterAnd')) : Q($this->gettext('filterOr')) ) );
+            $out .= $select_opt->show( ( $rule['opt']=='and' ?
+                Q( $this->gettext('filterAnd') ) : Q( $this->gettext('filterOr') ) ) );
         else
             $out .= $select_opt->show();
         $out .= '</td>';
@@ -430,7 +463,8 @@ class cpanel_filters extends rcube_plugin {
      * @param bool $div Include <div> wrapper
      * @return mixed XHTML output
      */
-    function parse_actions( $fid, $aid, $div = true ) {
+    function parse_actions( $fid, $aid, $div = true )
+    {
         $action = $this->filters[$fid]['actions'][$aid];
         $rows = sizeof($this->filters[$fid]['actions']);
         
@@ -461,15 +495,19 @@ class cpanel_filters extends rcube_plugin {
                 ( ( $action['action']=='deliver' || $action['action']=='fail' ||
                 !isset($action['action']) ) ? Q($action['dest'], 'strict', false) : '').
                 '" size="40" style="display:' .
-                ( ( $action['action']=='deliver' || $action['action']=='fail' ) ? 'inline' : 'none' ) . '" />';
+                ( ( $action['action']=='deliver' || $action['action']=='fail' ) ?
+                'inline' : 'none' ) . '" />';
         if ($action['action'] == 'save') {
             // TODO: add handler for windows slashes
             if ( end(explode('/',$action['dest'])) == 'INBOX' ) {
                 $mailbox = 'Inbox';
-            } else {
-                $mailbox = substr(end(explode('/',$action['dest'])),1,(strlen(end(explode('/',$action['dest'])))-1));
             }
-        } else {
+            else {
+                $mailbox = substr(end(explode('/',$action['dest'])),1,
+                        (strlen(end(explode('/',$action['dest'])))-1));
+            }
+        }
+        else {
             $mailbox = '';
         }
         $this->rcmail->imap_connect();
@@ -479,7 +517,8 @@ class cpanel_filters extends rcube_plugin {
 	        'id' => 'mailbox'.$aid,
 	        'name' => '_actions['.$aid.'][folder]',
 	        'style' => 'display:'.
-                ( ( $action['action']=='save' || !isset($action['action']) ) ? 'inline' : 'none' ),
+                ( ( $action['action']=='save' || !isset($action['action'] ) ) ?
+                'inline' : 'none' ),
 	    ));
         $out .= $select_box->show($mailbox);
         $out .= '</td>';
